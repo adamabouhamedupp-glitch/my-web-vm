01bfaa4c3144b78f4a745efbdb919764ce669b9e
adamabouhamedupp-glitch <adamabouhamedupp@gmail.com>
2026-02-11 11:40:38 +0200

    Update libv86.js

--- libv86.js
+++ libv86.js
@@ -1,1 +1,5 @@
-importScripts('https://cdn.jsdelivr.net/npm/v86@0.10.2/build/libv86.js');
+// Tiny Bootloader for v86
+console.log("Stealth Engine Starting...");
+const s = document.createElement("script");
+s.src = "https://unpkg.com/v86@0.10.2/build/libv86.js";
+document.head.appendChild(s);
